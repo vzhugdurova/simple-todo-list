dc3266de214619bac51dff5d2080d5483da88b04
Vika <zhuuvika@gmail.com>
2022-11-21 11:44:43 +0300

    added reducer and selectors for tasks

--- src/features/task/reducer.ts
+++ src/features/task/reducer.ts
@@ -1,3 +1,56 @@
-const tasksReducer = () => {};
+import { Reducer } from "redux";
+import TasksState from "./types/TaskState";
+import TasksAction from "./types/TaskAction";
+
+const initialState: TasksState = {
+  tasks: [],
+};
+
+const tasksReducer: Reducer<TasksState, TasksAction> = (
+  state = initialState,
+  { payload, type }
+) => {
+  switch (type) {
+    case "TASK_ADDED": {
+      return { ...state, tasks: [payload, ...state.tasks] };
+    }
+
+    case "TASKS_SORTED_BY_TIME": {
+      return {
+        ...state,
+        tasks: state.tasks.sort((a, b) => {
+          if (payload === "DESC") {
+            return b.timeToDo - a.timeToDo;
+          } else {
+            return a.timeToDo - b.timeToDo;
+          }
+        }),
+      };
+    }
+
+    case "TASKS_LOADED": {
+      return { ...state, tasks: payload };
+    }
+
+    case "TASK_DELETED": {
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== payload),
+      };
+    }
+
+    case "TASK_UPDATED": {
+      return {
+        ...state,
+        tasks: state.tasks.map((task) =>
+          task.id === payload.id ? payload : task
+        ),
+      };
+    }
+
+    default:
+      return state;
+  }
+};
 
 export default tasksReducer;
---
+++ src/features/task/selectors.ts
@@ -0,0 +1,5 @@
+import { RootState } from '../../store';
+import Task from './types/Task';
+
+export const selectTasks = (state: RootState): Task[] => state.tasks.tasks;
+export const selectTasksError = (state: RootState): string | undefined => state.tasks.error;
